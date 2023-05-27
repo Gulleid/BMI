0a84d81f088f8030a800858763603c718c7c5b24
Gulleid <gulleidmohamed93@gmail.com>
2023-05-27 15:39:56 -0800

    BMI Assigment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let aliHeight= 1.7;
+let aliWeight=67;
+let mohamedHeight=1.8;
+let mohamedWeight=72;
+
+BMIALI= aliWeight/aliHeight**2;
+console.log(BMIALI)
+Bmimohamed=mohamedHeight/mohamedWeight**2;
+console.log(Bmimohamed)
+
+if(BMIALI>Bmimohamed){
+    console.log("the bmi of ali is higher")
+} else console.log("mohamed is higher")
